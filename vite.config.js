820e15bad9e1f9a0507aadb45138571dd43edeee
dennisjunior247 <dennisjunior247@gmail.com>
2022-08-01 13:40:01 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			'svelte-apollo': '/node_modules/svelte-apollo/dist/svelte-apollo.es.js'
+	
+	vite: {
+		resolve: {
+			alias: {
+				'svelte-apollo': '/node_modules/svelte-apollo/dist/svelte-apollo.es.js'
+			}
 		}
 	}
 };
