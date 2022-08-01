ef3e0ad993cd53d2e76f472f8a3f865338b51ee8
dennisjunior247 <dennisjunior247@gmail.com>
2022-08-01 14:33:21 +0100

    fixes

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 
-	vite: {
-		resolve: {
-			alias: {
-				'svelte-apollo': '/node_modules/svelte-apollo/dist/svelte-apollo.es.js'
-			}
-		}
-	},
 	optimizeDeps: {
 		include: ['@apollo/client/core'],
-		exclude: ['@apollo/client', 'svelte-apollo']
+		exclude: ['@apollo/client']
 	},
 	ssr: {
 		noExternal: ['@apollo/client']
