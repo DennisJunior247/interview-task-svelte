d8e4de8c20403afaba7f09ab55f8d2e4135e7eaa
dennisjunior247 <dennisjunior247@gmail.com>
2022-08-02 08:07:44 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+
 	optimizeDeps: {
 		exclude: ['@apollo/client']
+	},
+	ssr: {
+		noExternal: ['@apollo/client']
 	}
 };
 
 export default config;
-
-// ssr: {
-// 	noExternal: ['@apollo/client']
-// }
