01df8ec70e49d53e72787f3796654d145dbffc59
dennisjunior247 <dennisjunior247@gmail.com>
2022-08-02 08:32:04 +0100

    fixed deployment bug

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,18 @@
 <script contex>
 	import '../app.css';
 
-	import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client/core';
+	import { ApolloClient, InMemoryCache } from '@apollo/client/core/core.cjs';
 	import { setClient } from 'svelte-apollo';
 
-	const httpLink = new HttpLink({
-		uri: 'https://countries.trevorblades.com/'
-	});
+	// const httpLink = new HttpLink({
+	// 	uri: 'https://countries.trevorblades.com/'
+	// });
 
 	const cache = new InMemoryCache();
 
 	const client = new ApolloClient({
 		cache: cache,
-		link: httpLink
+		uri: 'https://countries.trevorblades.com/'
 	});
 
 	setClient(client);
